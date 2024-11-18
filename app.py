5fc20487f92c8c3dbb0fde4814e6887fcf03c436
Saodat <3saodat@gmail.com>
2024-11-18 17:49:11 +0500

    first real development

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-print("Mohibaxon has taught me about chrome is not chrom")
-a= 3
-b = 2
-print( a+b )
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/check', methods=['POST'])
+def check():
+    url = request.form["url"]
+    result = f"The URL you entered is: {url}"
+    return render_template('index.html', result=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+

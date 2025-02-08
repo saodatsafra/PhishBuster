0b445cbec13388eca7b7f2b17f8006afe0823e9f
Saodat <3saodat@gmail.com>
2025-02-08 12:50:55 +0500

    SSL/TLS logic added

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template, request, jsonify
-import re
+import re #module
+import requests
 
 app = Flask(__name__)
 
+#Open homepage
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#Check SSL/TLS certificate validaty
+def check_ssl_certificate(url):
+    try:
+        response = requests.get(url, timeout=5)
+        return True
+    except requests.exceptions.SSLError:
+        return False
+    except requests.exceptions.RequestException:
+        return None #connection Error
+
+
+#Check the link
 @app.route('/check_link', methods=['POST'])
 def check_link():
     data = request.get_json()
     link = data.get('link', '')
 
-    # Check if the link is HTTPS or not
+    # Logic of checking the link for HTTPS or not
     if re.match(r'^https://', link):
         return jsonify({"status": "safe", "message": "The link is using HTTPS, which is generally secure."})
     elif re.match(r'^http://', link):
